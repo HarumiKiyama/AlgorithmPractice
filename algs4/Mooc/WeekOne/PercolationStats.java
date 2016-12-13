d10e4e3bd08cfa8c0a5020cbca7a7d1b822cb8df
uruk <lucius0720@hotmail.com>
2016-12-13 23:11:04 +0800

    Add mooc practice

---
+++ algs4/Mooc/WeekOne/PercolationStats.java
@@ -0,0 +1,30 @@
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+
+public class PercolationStates {
+    public PercolationStates(int n, int trials){
+        if(n<=0||trials<=0){
+            throw java.lang.IllegalArgumentException();
+        }
+    }
+    public double mean(){
+
+    }
+    public double stddev(){
+        
+    }
+    public double confidenceLo(){
+        
+    }
+    public double confidenceHi(){
+        
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
