fd870dcdabd525e6e170df2af5de8dc328458be2
uruk <lucius0720@hotmail.com>
2017-06-18 23:55:13 +0800

    codewar haskell

---
+++ Haskell/directions_reduction.hs
@@ -0,0 +1,26 @@
+data Direction
+  = North
+  | East
+  | West
+  | South
+  deriving (Eq,Show)
+
+
+
+
+
+dirReduce :: [Direction] -> [Direction]
+dirReduce [] = []
+dirReduce (x:xs) = sequenceDir (dirReduce xs) x
+  where sequenceDir :: [Direction] -> Direction -> [Direction]
+        sequenceDir [] dir = dir : []
+        sequenceDir xs dir =
+          case mergeDirec [head xs, dir] of
+            [] -> tail xs
+            _ -> dir : xs
+            where mergeDirec :: [Direction] -> [Direction]
+                  mergeDirec [North, South] = []
+                  mergeDirec [South, North] = []
+                  mergeDirec [West, East] = []
+                  mergeDirec [East, West] = []
+                  mergeDirec xs = xs
