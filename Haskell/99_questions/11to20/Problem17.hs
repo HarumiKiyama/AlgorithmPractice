bb9c788fbd400c8c42eaf5643c85f5e991d199c6
uruk <lucius0720@hotmail.com>
2017-07-02 00:22:41 +0800

    modify Haskell Problem17

--- Haskell/99_questions/11to20/Problem17.hs
+++ Haskell/99_questions/11to20/Problem17.hs
@@ -1,14 +1,21 @@
 module Problem17 where
 
 -- split a list into two parts
-split :: [a] -> Int -> [[a]]
-split xs n = [firstPart xs n, secondPart xs n]
+split :: [a] -> Int -> ([a], [a])
+split xs n = (firstPart xs n, secondPart xs n)
   where
     firstPart :: [a] -> Int -> [a]
-    firstPart [] _       = []
-    firstPart _ 0        = []
+    firstPart [] _ = []
+    firstPart _ 0 = []
     firstPart (y:ys) num = y : firstPart ys (num - 1)
     secondPart :: [a] -> Int -> [a]
-    secondPart ys 0       = ys
-    secondPart [] _       = []
+    secondPart ys 0 = ys
+    secondPart [] _ = []
     secondPart (_:ys) num = secondPart ys (num - 1)
+
+split' :: [a] -> Int -> ([a], [a])
+split' (x:xs) n
+  | n > 0 =
+    let (f, l) = split' xs (n - 1)
+    in (x : f, l)
+split' xs _ = ([], xs)
