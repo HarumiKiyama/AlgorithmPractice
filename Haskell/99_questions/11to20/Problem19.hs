0ddbed93a65bd433b46354f61ebccf7dfa67d39f
uruk <lucius0720@hotmail.com>
2017-07-02 23:04:32 +0800

    Summary: fix some problems in problem19
    language: Haskell
    Detail: fix some problems in problems19

--- Haskell/99_questions/11to20/Problem19.hs
+++ Haskell/99_questions/11to20/Problem19.hs
@@ -3,8 +3,10 @@ module Problem18 where
 -- Rotate a list N places to the left.
 
 rotate :: [a] -> Int -> [a]
+rotate [] _ = []
 rotate xs n
   | n < 0 =
     let position = length xs + n
     in drop position xs ++ take position xs
   | n > 0 = drop n xs ++ take n xs
+  | n == 0 = xs
