3c408179a5a230717ca23091b5821253600777c7
uruk <lucius0720@hotmail.com>
2017-07-08 23:10:25 +0800

    Summary: Haskell Problem 28
    language: Haskell
    Detail:

--- Haskell/99_questions/21to28/Problem28.hs
+++ Haskell/99_questions/21to28/Problem28.hs
@@ -1,4 +1,6 @@
 module Problem28 where
+import Data.Function
+import Data.List
 
 -- We suppose that a list contains elements that are lists themselves. The objective is to sort the elements of this list according to their length. E.g. short lists first, longer lists later, or vice versa.
 lsort :: [[a]] -> [[a]]
@@ -12,4 +14,4 @@ lsort (x:xs) = f ++ [x] ++ l
 -- Again, we suppose that a list contains elements that are lists themselves. But this time the objective is to sort the elements of this list according to their length frequency; i.e., in the default, where sorting is done ascendingly, lists with rare lengths are placed first, others with a more frequent length come later.
 
 lfsort :: [[a]]->[[a]]
-lfsort = id
+lfsort = concat . lsort . groupBy ((==) `on` length) . lsort
