b9739646229a108399e3b8e8efb988c8c35a79be
uruk <lucius0720@hotmail.com>
2017-09-16 23:22:40 +0800

    Summary: complete hamming
    language: elisp

--- elisp/exerism/hamming/hamming.el
+++ elisp/exerism/hamming/hamming.el
@@ -6,7 +6,15 @@
 
 (require 'cl)
 
+(defun hamming-distance (f s)
+  (if (= (length f) (length s))
+      (apply '+ (mapcar* 'hamming-diff f s))
+    (error "These two DNAs are not aligned"))
+  )
 
+(defun hamming-diff (f s)
+  (if (= f s) 0 1)
+  )
 
 (provide 'hamming)
 ;;; hamming.el ends here
